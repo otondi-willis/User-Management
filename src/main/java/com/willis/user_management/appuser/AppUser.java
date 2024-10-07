376bd3fe2dae6346e7a11b19d6fbda308c101344
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-07 04:51:58 +0300

    feat:Added a parameterized constructor

--- src/main/java/com/willis/user_management/appuser/AppUser.java
+++ src/main/java/com/willis/user_management/appuser/AppUser.java
@@ -22,6 +22,22 @@ public class AppUser implements UserDetails {
     private Boolean locked;
     private Boolean enabled;
 
+    public AppUser(String name,
+                   String username,
+                   String email,
+                   String password,
+                   AppUserRole appUserRole,
+                   Boolean locked,
+                   Boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority =
