2b0802502949e555e91d7ff8369ec13fb2695734
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-07 04:37:34 +0300

    feat:Created AppUser class with corresponding attributes

---
+++ src/main/java/com/willis/user_management/appuser/AppUser.java
@@ -0,0 +1,34 @@
+package com.willis.user_management.appuser;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class AppUser implements UserDetails {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private AppUserRole appUserRole;
+    private Boolean isLocked;
+    private Boolean isEnabled;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+}
