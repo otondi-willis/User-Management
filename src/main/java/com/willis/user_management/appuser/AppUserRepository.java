c237324489c711221cfc2a6f08e07e26f9a167d4
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-08 03:36:54 +0300

    feat:Added  findByEmail method

---
+++ src/main/java/com/willis/user_management/appuser/AppUserRepository.java
@@ -0,0 +1,14 @@
+package com.willis.user_management.appuser;
+
+
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+    Optional<AppUser> findByEmail(String email);
+}
