e293dbfb9273415fec208c23c9ff92876b360922
otondi-willis <willis.otondi.ke@gmail.com>
2024-10-08 03:19:56 +0300

    feat:Added AppUserService that implements UserDetailsService.

---
+++ src/main/java/com/willis/user_management/appuser/AppUserService.java
@@ -0,0 +1,13 @@
+package com.willis.user_management.appuser;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
